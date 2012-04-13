24909b1d37f9fae25d2cf0361663917261defc8b
Felipe González <felipexgonzalez@gmail.com>
2012-04-12 22:06:30 -0500

    Agregar resúmenes de las corridas

--- src/1_modelo_simple_2vars.R
+++ src/1_modelo_simple_2vars.R
@@ -2,7 +2,9 @@
 ## Referencia: Kruschke. Doing Bayesian data analysis.
 
 tabla.1 <- data.frame(HairEyeColor)
-tabla.h <- subset(tabla.1, Sex == 'Male', select = -Sex)
+tabla.h <- subset(tabla.1, Sex == 'Female', select = -Sex)
+
+#tabla.h$Freq <- round(tabla.h$Freq/3)
 
 pelo <- as.numeric(tabla.h$Hair)
 n.pelo <- length(levels(tabla.h$Hair))
@@ -20,4 +22,18 @@ fit.poisson <- jags(model.file = model.file,
    n.chains = 3, n.burnin = 500, n.thin =1, DIC = FALSE,
    n.iter = 2000)
 
+# ================
+# = Simulaciones =
+# ================
 
+sims.n <- fit.poisson$BUGSoutput$sims.list$sim.n
+
+
+sims.m <- melt(sims.n)
+colnames(sims.m) <- c('celda', 'sim.num', 'Frec.sim')
+tabla.2 <- data.frame(sim.num = 1:nrow(tabla.h), tabla.h)
+sims.m.2 <- join(sims.m, tabla.2, by ='sim.num', type = 'left')
+
+resumen.1 <- ddply(sims.m.2, c('Hair', 'Eye'), summarise,
+   sim.media = mean(Frec.sim), sim.sd = sd(Frec.sim),
+   obs= mean(Freq))
